92660ca258962f303d5fd7c547600a8523756ca9
Fahad <alfahad360@gmail.com>
2023-07-16 23:04:08 +0100

    added bg image. menu links and rimary logo

--- app/page.js
+++ app/page.js
@@ -1,28 +1,45 @@
-import Image from "next/image";
 import styles from "./page.module.scss";
-
+import Image from "next/image";
+import primaryLogo from "../assets/icons/paloma_logo_primary.svg";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div>
+        <Image
+          src={primaryLogo}
+          alt="paloma logo"
+          priority={true}
+          className={styles.primaryLogo}
+        />
       </div>
+      <nav className={styles.mainMenuNavigation}>
+        <ul>
+          <li>
+            <a href="#">hear</a>
+          </li>
+          <li>
+            <a href="#">latest</a>
+          </li>
+          <li>
+            <a href="#">tour</a>
+          </li>
+          <li>
+            <a href="#">see</a>
+          </li>
+          <li>
+            <a href="#">shop</a>
+          </li>
+          <li>
+            <a href="#">watch</a>
+          </li>
+          <li>
+            <a href="#">personal</a>
+          </li>
+          <li>
+            <a href="#">aid</a>
+          </li>
+        </ul>
+      </nav>
     </main>
   );
 }
